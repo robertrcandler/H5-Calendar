e490cede4df40f63508f00a45d6507faae4dbd35
Robert Candler <robert.candler.32@gmail.com>
2020-03-26 19:44:56 -0600

    decent looking first row

---
+++ logic.js
@@ -0,0 +1,52 @@
+
+//make variables for the current date and time
+var rn = new Date();
+var currentHour = rn.getHours();    //16>militarytime
+var realDate = rn.getDate();     //26>Day of the month,does not start at 0
+var currentMonth = rn.getMonth();   //2>March
+var realMonth = "";
+switch (currentMonth) {
+  case 0:
+    realMonth = "January";
+    break;
+  case 1:
+    realMonth = "February";
+    break;
+  case 2:
+    realMonth = "March";
+    break;
+  case 3:
+    realMonth = "April";
+    break;
+  case 4:
+    realMonth = "May";
+    break;
+  case 5:
+    realMonth = "June";
+    break;
+  case 6:
+    realMonth = "July";
+    break;
+  case 7:
+    realMonth = "Augest";
+    break;
+  case 8:
+    realMonth = "September";
+    break;
+  case 9:
+    realMonth = "October";
+    break;
+  case 10:
+    realMonth = "November";
+    break;
+  case 11:
+    realMonth = "December";
+    break;
+}
+var realYear = rn.getFullYear(); //2020
+
+//display the current date in jumbotron
+var displayDate = document.getElementById("currentDay");
+displayDate.textContent = realMonth+" "+realDate+", "+realYear;
+
+
